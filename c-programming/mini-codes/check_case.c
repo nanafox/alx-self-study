ce267a7eb66a2285d461c3b263512b74f5226986
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-04-30 07:21:04 +0000

    Few fixes

--- c-programming/mini-codes/check_case.c
+++ c-programming/mini-codes/check_case.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
 /// @author theLazyProgrammer^_^
-/// @brief Letter case checker. What case is this?
-/// @param letter A single letter to test case of
-/// @note Prints out the correspoding case.
+/// @brief Letter case checker.
+/// @param letter A single letter to check case.
+/// @note This function prints out the correspoding case.
+/// @returns void(nothing)
 void check_case(char letter)
 {
-    int casing;
-
     // upper case letter ASCII range
     if (letter >= 65 && letter < 90)
-        printf("%c is Upper case\n", letter);
+        printf("%c is UPPER CASE\n", letter);
     // check for lower case
     else if (letter >= 97 && letter < 122)
         printf("%c is lower case\n", letter);
     // check everything else
     else
-        printf("%c is not a recognized alphabetic.\n", letter);
+        printf("%c is not a recognized alphabet.\n", letter);
 }
 
 int main(void)
