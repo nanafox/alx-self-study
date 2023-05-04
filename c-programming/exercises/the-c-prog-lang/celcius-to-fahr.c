8f63e05ee65667572e8e82cc20d5fa2fd478f345
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-05-04 12:54:41 +0000

    changed the return type to float, makes things simpler and doesn't truncate significant precisions

--- c-programming/exercises/the-c-prog-lang/celcius-to-fahr.c
+++ c-programming/exercises/the-c-prog-lang/celcius-to-fahr.c
@@ -6,24 +6,10 @@
 
 #include <stdio.h>
 
-int celcius_to_fahr(float celcius_temperature); /*Covert celcius to farhenheit*/
-int fahr_to_celcius(float fahr_temperature);    /*Convert farhenheit to celcius*/
-
-int main(void)
-{
-    float fahr = celcius_to_fahr(45);
-    float celcius = fahr_to_celcius(fahr); /*Covert the fahr value received*/
-
-    printf("Fahrenheit temperature: %.2f°F\n", fahr);
-    printf("Celcius temperature: %.2f°C\n", celcius);
-
-    return 0;
-}
-
 /// @brief Performs fahrenheit to celcius conversion
 /// @param celcius_temperature The temperature in Fahrenheit
 /// @returns Temperature in Farhenheit
-int celcius_to_fahr(float celcius_temperature)
+float celcius_to_fahr(float celcius_temperature)
 {
     // compute fahr from celcius
     return (celcius_temperature * 9 / 5) + 32;
@@ -32,7 +18,7 @@ int celcius_to_fahr(float celcius_temperature)
 /// @brief Performs fahrenheit to celcius conversion
 /// @param fahr_temperature The temperature in Fahrenheit
 /// @returns Temperature in Celcius
-int fahr_to_celcius(float fahr_temperature)
+float fahr_to_celcius(float fahr_temperature)
 {
     // compute fahr from celcius
     return 5 * (fahr_temperature - 32) / 9;
