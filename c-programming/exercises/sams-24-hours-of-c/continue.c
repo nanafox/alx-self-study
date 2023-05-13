e631933e6c453eaf3ed869978d1ff097e9b5cebb
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-05-13 08:32:28 +0000

    fixed typo in comments

--- c-programming/exercises/sams-24-hours-of-c/continue.c
+++ c-programming/exercises/sams-24-hours-of-c/continue.c
@@ -2,13 +2,13 @@
 
 int main(void)
 {
-    /*Using the continue statement to skip to the beginning of the lop*/
+    /*Using the continue statement to skip to the beginning of the loop*/
     printf("Multiples of 3: ");
 
     for (int i = 1; i <= 30; i++)
     {
         if (i % 3 != 0)
-            continue; // skip any number that's not a multiple of 2
+            continue; // skip any number that's not a multiple of 3
 
         printf("%d ", i); // print multiples of 3
     }
