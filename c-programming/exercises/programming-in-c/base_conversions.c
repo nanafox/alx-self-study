95b21a53d196ebc6651c4a15ebc240bac95cd1b2
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-05-29 04:25:08 +0000

    added to_base8 function

--- c-programming/exercises/programming-in-c/base_conversions.c
+++ c-programming/exercises/programming-in-c/base_conversions.c
@@ -44,7 +44,7 @@ void to_base2(unsigned int num)
 */
 void to_base16(unsigned int num)
 {
-	char base16[16] = {
+	const char base16[16] = {
 		'0', '1', '2', '3', '4', '5', '6', '7',
 		'8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
 	};
@@ -67,5 +67,30 @@ void to_base16(unsigned int num)
 		printf("%c", base16[value]);
 	}
 
+	putchar('\n');
+}
+
+/**
+ * to_base8 - Converts a number to base 8 numeral
+ *
+ * @num: number to convert to base 8
+*/
+void to_base8(unsigned int num)
+{
+	int octal[32];
+	int index = 0;
+
+	while (num != 0)
+	{
+		octal[index] = num % 8; /*record remainder*/
+		index++;
+		num /= 8; /*get quotient*/
+	}
+
+	/*print result in reverse order*/
+	for (--index; index >= 0; --index)
+	{
+		printf("%d", octal[index]);
+	}
 	putchar('\n');
 }
