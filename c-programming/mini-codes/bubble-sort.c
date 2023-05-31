da54bb176c78c850650c3616e163856fd0ac4ef9
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-05-31 04:44:45 +0000

    added a user interaction component

--- c-programming/mini-codes/bubble-sort.c
+++ c-programming/mini-codes/bubble-sort.c
@@ -8,17 +8,26 @@
 int main(void)
 {
 	int i, temp, swapped;
-	int numbers[5] = {5, 3, 10, 2, 1};
+	int size = 5;
+	int numbers[size];
+
+	printf("bubSort: Enter 5 numbers that needs to be sorted.\n\n");
+	/*get numbers from user - this forms the original which will be later sorted*/
+	for (i = 0; i < size; i++)
+	{
+		printf("Value #%d: ", i + 1);
+		scanf("%d", &numbers[i]);
+	}
 
 	/*print original list*/
-	printf("Original list: ");
-	for (i = 0; i < 5; i++)
+	printf("\nOriginal list: ");
+	for (i = 0; i < size; i++)
 		printf("%d ", numbers[i]);
 
-	/*perform sort*/
+	/*perform bubble sort*/
 	do {
 		swapped = 0;
-		for (i = 0; i < 4; i++)
+		for (i = 0; i < size - 1; i++)
 			if (numbers[i] > numbers[i + 1])
 			{
 				temp = numbers[i]; /*temporary storage*/
@@ -42,5 +51,5 @@ int main(void)
  * Observation
  *
   * the range excludes the higher bound since it bubbles and should be
-  * alone. This way, we can focus on the numbers that still low.
+  * left alone. This way, we can focus on the numbers that are still low.
 */
