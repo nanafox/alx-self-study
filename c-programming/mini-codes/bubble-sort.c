457879cd87ad7b7ea2e26f7ae0115512f4e0ad53
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-05-31 05:04:45 +0000

    bubble sort with bit manipulation for value swapping

--- c-programming/mini-codes/bubble-sort.c
+++ c-programming/mini-codes/bubble-sort.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - sorting with the bubble sort algorithm
+ * main - sorting with the bubble sort algorithm (for integers only).
  *
  * Return: 0
 */
 int main(void)
 {
-	int i, temp, swapped;
-	int size = 5;
+	int i, swapped;
+	int size = 5; /*number of items to store in array / sort*/
 	int numbers[size];
 
-	printf("bubSort: Enter 5 numbers that needs to be sorted.\n\n");
+	printf("bubSort: Enter %d numbers that needs to be sorted.\n\n", size);
 	/*get numbers from user - this forms the original which will be later sorted*/
 	for (i = 0; i < size; i++)
 	{
@@ -22,7 +22,8 @@ int main(void)
 	/*print original list*/
 	printf("\nOriginal list: ");
 	for (i = 0; i < size; i++)
-		printf("%d ", numbers[i]);
+		/*print element while taking care of spacing*/
+		(i < 5) ? printf("%d ", numbers[i]) : printf("%d", numbers[i]);
 
 	/*perform bubble sort*/
 	do {
@@ -30,9 +31,9 @@ int main(void)
 		for (i = 0; i < size - 1; i++)
 			if (numbers[i] > numbers[i + 1])
 			{
-				temp = numbers[i]; /*temporary storage*/
-				numbers[i] = numbers[i + 1]; /*push lower number left*/
-				numbers[i + 1] = temp; /*bubble higher number to the right*/
+				numbers[i + 1] ^= numbers[i]; /*temporary storage*/
+				numbers[i] ^= numbers[i + 1]; /*push lower number left (swap)*/
+				numbers[i + 1] ^= numbers[i]; /*bubble higher number to the right*/
 				swapped = 1; /*confirm swap ocurred*/
 			}
 	} while (swapped); /*run so long as there are elements to sort.*/
@@ -40,7 +41,8 @@ int main(void)
 	/*print sorted list*/
 	printf("\nSorted list: ");
 	for (i = 0; i < 5; i++)
-		printf("%d ", numbers[i]);
+		/*print element while taking care of spacing*/
+		(i < 5) ? printf("%d ", numbers[i]) : printf("%d", numbers[i]);
 
 	putchar('\n');
 
@@ -48,8 +50,13 @@ int main(void)
 }
 
 /*
- * Observation
+ * Observation - May 30, 2023
  *
   * the range excludes the higher bound since it bubbles and should be
   * left alone. This way, we can focus on the numbers that are still low.
+  *
+ * Update - May 31, 2023
+ *
+  * Used bit manipulation to swap the values and got rid of the extra
+  * variable, temp.
 */
