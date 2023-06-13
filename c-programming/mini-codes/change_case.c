d8bab582ade710fecbe9f81e06ebed9ed3cf3cb1
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-06-13 04:21:55 +0000

    betty-compliant

--- c-programming/mini-codes/change_case.c
+++ c-programming/mini-codes/change_case.c
@@ -1,24 +1,30 @@
-#include <stdio.h>
-
 /*The lower and upper case letters are 32 numbers apart.*/
-const int low_upper_interval = 32;
+#define low_upper_interval 32
 
-/// @author theLazyProgrammer^_^
-/// @brief Converts upper case to lower case
-/// @param letter The letter you are testing
-/// @return lowercase version of letter. i.e. letter + 32
-/// @note The lower and upper case letters are 32 numbers apart.
+/**
+ * to_lower - Converts upper case to lower case
+ * @letter: The letter you are testing
+ *
+ * Note: The lower and upper case letters are 32 numbers apart.
+ * Author: theLazyProgrammer^_^
+ *
+ * Return: lowercase version of letter. i.e. letter + 32
+ */
 char to_lower(char letter)
 {
-    if (letter >= 65 && letter < 90)
-        return letter + low_upper_interval;
+	if (letter >= 65 && letter < 90)
+		return (letter + low_upper_interval);
 }
 
-/// @brief Coverts lower case to upper case
-/// @param letter The letter you are testing
-/// @return uppercase version of letter. i.e. letter - 32
+/**
+ * to_upper - Coverts lower case to upper case
+ * @letter: The letter you are testing
+ *
+ * Return: uppercase version of letter. i.e. letter - 32
+ */
 char to_upper(char letter)
 {
-    if (letter >= 97 && letter < 122)
-        return letter - low_upper_interval;
+	if (letter >= 97 && letter < 122)
+		return (letter - low_upper_interval);
 }
+
