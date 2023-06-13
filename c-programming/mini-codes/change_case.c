5cfc0b27cb61bd7d4d20f23864164ce4ec3f2fa3
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-06-13 06:28:08 +0000

    added function for string title casing

--- c-programming/mini-codes/change_case.c
+++ c-programming/mini-codes/change_case.c
@@ -12,8 +12,20 @@
  */
 char to_lower(char letter)
 {
-	if (letter >= 65 && letter < 90)
+	if (letter >= 'A' && letter < 'Z')
 		return (letter + low_upper_interval);
+}
+
+/**
+ * is_lower - my version of ctype()'s islower() function.
+ *
+ * @string: string to check
+ *
+ * Return: 1 if lower case, else 0.
+ */
+int is_lower(char *string)
+{
+	/*TODO*/
 }
 
 /**
@@ -24,7 +36,35 @@ char to_lower(char letter)
  */
 char to_upper(char letter)
 {
-	if (letter >= 97 && letter < 122)
+	if (letter >= 'a' && letter < 'z')
 		return (letter - low_upper_interval);
 }
 
+/**
+ * to_title - Convert string to Title Case
+ *
+ * @string: text to convert
+ *
+ * Note: It modifies the original string's value.
+ *
+ * Return: title case string.
+*/
+char *to_title(char *string)
+{
+	for (int i = 0; string[i] != '\0'; i++)
+	{
+		if (i == 0)
+			/*capitalize the first letter in the string*/
+			string[i] = to_upper(string[i]);
+
+		else if (string[i - 1] == ' ')
+			/*change letter to upper case after each space*/
+			string[i] = to_upper(string[i]);
+
+		else
+			/*lower all other characters*/
+			string[i] = to_lower(string[i]);
+	}
+
+	return (string);
+}
