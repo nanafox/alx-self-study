1ecb42c539f55f4e4289e0412cc70184c608d0c5
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-08-23 23:29:03 +0000

    updates

--- c-programming/exercises/the-c-prog-lang/ch03-control-flow/ex3.5-itob.c
+++ c-programming/exercises/the-c-prog-lang/ch03-control-flow/ex3.5-itob.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
 #include "_reverse.c"
+#include <stdio.h>
 #include <stdlib.h>
 
 void _itob(int n, char *s, int b);
 
 /**
  * main - integer to any base
+ *
+ * @argc: arg count
+ * @argv: requires a number and the base
  *
  * Return: 0
  */
@@ -16,13 +19,11 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Usage: %s number to_base\n", argv[0]);
 		return (1);
 	}
-	char *s = malloc(sizeof(char));
+	char s[10];
 
 	_itob(atoi(argv[1]), s, atoi(argv[2]));
 	printf("Base 10: %d\n", atoi(argv[1]));
 	printf("Base %d: %s\n", atoi(argv[2]), s);
-
-	free(s);
 
 	return (0);
 }
@@ -32,27 +33,37 @@ int main(int argc, char **argv)
  * as a string in @s
  *
  * @n: number
- * @s: malloc'd string to hold representation
+ * @s: string to hold representation
  * @b: base to convert @n to
  */
 void _itob(int n, char *s, int b)
 {
-	int i = 0;
+	int i = 0, sign = n;
+	
+	if (n < 0)
+		n = -n;
 
 	switch (b) /* check base number */
 	{
-		case 2: case 8: case 10: case 16: /* accepted base */
+	/* accepted base */
+	case 2:
+	case 8:
+	case 10:
+	case 16:
 		do {
 			s[i++] = (n % b > 9) ? 'A' + (n % b - 10) : (n % b + '0');
-			s = realloc(s, sizeof(char) * 2); /* ask for more memory */
 		} while ((n /= b) > 0);
 
+		if (sign < 0)
+		{
+			s[i++] = '-';
+		}
 		s[i] = '\0';
 		_reverse(s); /* get the correct representation */
 		break;
 
-		default:
-			fprintf(stderr, "Invalid base number!\n");
-			return;
+	default:
+		fprintf(stderr, "Invalid base number!\n");
+		return;
 	}
 }
