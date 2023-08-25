a3a05f918ec0fef3de526a69dddcf3a3ee19596e
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-08-25 21:02:58 +0000

    added error handling

--- c-programming/mini-codes/guessing-game.py
+++ c-programming/mini-codes/guessing-game.py
@@ -12,26 +12,35 @@
 # give hints if higher or lower
 
 import random
+import sys
 
-def get_random_number():
-    number_A = int(input("Point A: "))
-    number_B = int(input("Point B: "))
-
-    if number_A >= number_B:
+def get_random_number(point_A, point_B):
+    if point_A >= point_B:
         print("Invalid range.. A is greater B")
     else:
-        return random.randint(number_A, number_B)
+        return random.randint(point_A, point_B)
 
 def number_guessing_game():
-    guess = get_random_number()
+    try:
+        number_A = int(input("Point A: "))
+        number_B = int(input("Point B: "))
+    except KeyboardInterrupt:
+        sys.exit("Ctrl + C received")
+    except ValueError:
+        sys.exit("Enter only integer values")
+    guess = get_random_number(number_A, number_B)
 
     if guess is not None:
-        allowed_guesses = 5
+        allowed_guesses = number_B
 
         while allowed_guesses > 0:
             print(f"\nChances left: {allowed_guesses}\n")
-            user_guess = int(input("Your guess: "))
-
+            try:
+                user_guess = int(input("Your guess: "))
+            except KeyboardInterrupt:
+                sys.exit("Ctrl + C received")
+            except ValueError:
+                sys.exit("Enter only integer values")
             allowed_guesses -= 1
             if user_guess > guess:
                 print("Too high")
