30e9d0191d5b3c534d16ccd8c081dceca4f8a2ea
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-19 16:05:31 +0000

    printing decimals recursively

---
+++ c-programming/exercises/the-c-prog-lang/ch04-functions-and-program-structure/printd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * printd print n in decimal
+ * @n: number
+ */
+void printd(int n) 
+{ 
+	if (n < 0)
+	{ 
+		putchar('-'); 
+		n = -n; 
+		if (n / 10) 
+			printd(n / 10); 
+		putchar(n % 10 + '0');
+	}
+}
