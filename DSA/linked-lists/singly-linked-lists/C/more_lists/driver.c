e1138093940bf85cfffcfa9053920f9bdb970805
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-22 15:33:13 +0000

    Updated with test code for rm operation

--- DSA/linked-lists/singly-linked-lists/C/more_lists/driver.c
+++ DSA/linked-lists/singly-linked-lists/C/more_lists/driver.c
@@ -10,22 +10,35 @@ int main(void)
 	list *my_list = init(_free);
 	int data;
 
+	puts("Insertions");
 	insert(my_list, 0, 45);
 	insert(my_list, -1, 23);
 	insert(my_list, 0, 81);
 	insert(my_list, 1, 67);
+
+	puts("\nElements");
 	printf("first number: %d\n", first(my_list));
 	printf("last number: %d\n", last(my_list));
 	printf("All items\n");
 	print_list(my_list);
 	printf("Size: %ld\n", len(my_list));
 
+	puts("\nPop operation");
 	data = pop(my_list);
 	printf("Removed: [%d]\n", data);
 	print_list(my_list);
 	printf("first number: %d\n", first(my_list));
 	printf("last number: %d\n", last(my_list));
 
+	puts("\nSpecific element removal");
+	if (rm(my_list, 67) == 0)
+		printf("Element removed\n");
+	else
+		printf("An error occured while removing element\n");
+
+	print_list(my_list);
+
+	puts("\nClearing list and trying to insert afterwards");
 	clear(my_list);
 	my_list = NULL;
 	append(my_list, 45);
