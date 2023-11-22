3181fb4ca038ba5ad7bb0e621f609b0fa8af4d7f
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-22 15:32:35 +0000

    Added a function to remove a specific element

--- DSA/linked-lists/singly-linked-lists/C/more_lists/deletions.c
+++ DSA/linked-lists/singly-linked-lists/C/more_lists/deletions.c
@@ -39,3 +39,42 @@ int pop(list *user_list)
 
 	return (data);
 }
+
+/**
+ * rm - removes a the first occurrence of the element from a list
+ * @user_list: the list to remove from
+ * @data: the element to remove
+ *
+ * Return: 0 on success, else -1 on error
+ */
+int rm(list *user_list, const int data)
+{
+	node *current = NULL, *prev = NULL;
+
+	if (user_list->head == NULL || user_list == NULL)
+	{
+		fprintf(stderr, "List is empty or uninitialized\n");
+		return (-1); /* nothing to remove */
+	}
+
+	current = user_list->head;
+	while (current != NULL)
+	{
+		if (current->data == data)
+		{
+			if (current == user_list->head)
+				user_list->head = user_list->head->next;
+			else
+				prev->next = current->next;
+
+			/* clean up memory for old node */
+			user_list->cleanup((void **)&current);
+			return (0); /* element removal worked */
+		}
+		prev = current;
+		current = current->next;
+	}
+
+	return (-1);
+}
+
