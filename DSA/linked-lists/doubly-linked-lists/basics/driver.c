eb05523e89a78fab9dfdf4a13647f4a6a4212d13
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-23 12:24:01 +0000

    Code cleanup and extra feature testing

--- DSA/linked-lists/doubly-linked-lists/basics/driver.c
+++ DSA/linked-lists/doubly-linked-lists/basics/driver.c
@@ -8,39 +8,22 @@
 int main(void)
 {
 	dll_t *my_list = init(_free);
-	node *tmp;
 	int i, elements[10] = {5, 3, 23, 6, 12, 9, 22, 90, 44, 15};
-
-	if (my_list == NULL)
-		return (-1);
 
 	for (i = 0; i < 10; i++)
 	{
 		if (append(my_list, elements[i]) == -1)
 		{
-			fprintf(stderr, "Couldn't insert data");
+			fprintf(stderr, "Couldn't insert data\n");
 			break;
 		}
 	}
 
-	tmp = my_list->head;
-	i = 0;
-	while (tmp != NULL)
-	{
-		printf("[%d]: %d\n", ++i, tmp->data);
-		tmp = tmp->next;
-	}
-	printf("Size of List: %lu\n", my_list->size);
-
-	/* free memory for the list */
-	while (my_list->head != NULL)
-	{
-		tmp = my_list->head;
-		my_list->head = my_list->head->next;
-
-		my_list->cleanup((void **)&tmp);
-	}
-	my_list->cleanup((void **)&my_list);
+	insert(my_list, 1, 4521);
+	insert(my_list, 4, 1);
+	insert(my_list, 8, 9);
+	print_list(my_list);
+	clear(my_list);
 
 	return (0);
 }
