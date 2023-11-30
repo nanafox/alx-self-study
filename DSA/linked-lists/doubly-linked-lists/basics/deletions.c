35f91a1bb6ba4647c40d510e1c68df30fe50d9f2
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-30 08:00:52 +0000

    Added deletion logic for anywhere in the list

--- DSA/linked-lists/doubly-linked-lists/basics/deletions.c
+++ DSA/linked-lists/doubly-linked-lists/basics/deletions.c
@@ -26,10 +26,18 @@ int pop(dll_t *list)
 
 	/* clean up the memory used by the deleted node */
 	list->cleanup((void **)&temp_tail);
+	list->size--;
 
 	return (data);
 }
 
+/**
+ * rm - removes the first occurrence of an element from a list
+ * @list: the list to remove from
+ * @data: the data to remove
+ *
+ * Return: 0 on success, else -1 on failure
+ */
 int rm(dll_t *list, const int data)
 {
 	node *current;
@@ -37,18 +45,39 @@ int rm(dll_t *list, const int data)
 	if (check_err(list, 'l') == -1 || check_err(list->head, 'm') == -1)
 		return (-1);
 
+	current = list->head;
+
+	/* handle deletion at the beginning */
 	if (list->head->data == data)
 	{
-		current = list->head;
 		list->head = list->head->next;
 		if (list->head != NULL)
 			list->head->prev = NULL;
-	
-		list->cleanup((void **)&current);
-		return (0);
+		else
+		{
+			list->head = NULL;
+			list->tail = NULL;
+		}
+	}
+	else
+	{
+		while (current != NULL && current->data != data)
+			current = current->next;
+
+		if (current == NULL || current->data != data)
+		{
+			fprintf(stderr, "'%d' not found in the list\n", data);
+			return (-1);
+		}
+
+		if (current->prev != NULL)
+			current->prev->next = current->next;
+
+		if (current->next != NULL)
+			current->next->prev = current->prev;
 	}
 
-	/* deleting somewhere in the list */
-
-	return (-1);
+	list->cleanup((void **)&current);
+	list->size--;
+	return (0);
 }
