f5385a1c13060550ab54c140d4f559151fb4abf9
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-30 08:01:34 +0000

    Added more test codes and modularized them

--- DSA/linked-lists/doubly-linked-lists/basics/driver.c
+++ DSA/linked-lists/doubly-linked-lists/basics/driver.c
@@ -1,6 +1,47 @@
 #include "dll.h"
 
 /**
+ * perform_insertions - performs multiple insertions into a list
+ * @list: the list to insert into
+ */
+void perform_insertions(dll_t *list)
+{
+	/* insert some more */
+	insert(list, 1, 4521);
+	insert(list, 4, 1);
+	insert(list, -3, 78);
+	append(list, 100);
+	insert(list, -4, 500);
+	insert(list, 0, 128);
+
+	/* print the updated list */
+	puts("\nAfter insertion\n");
+	print_list(list);
+}
+
+/**
+ * perform_deletions - performs multiple deletions from a list
+ * @list: the list to delete from
+ */
+void perform_deletions(dll_t *list)
+{
+	printf("\nElement [%d] was popped from the list\n", pop(list));
+	puts("After pop operation\n");
+	print_list(list);
+
+	puts("\nRemoving data\n");
+	rm(list, 5);
+	print_list(list);
+
+	printf("\nRemoving within the list\n");
+	rm(list, 500);
+	rm(list, 23);
+	rm(list, 1024);
+
+	print_list(list);
+}
+
+/**
  * main - a driver code to test doubly linked list operations
  *
  * Return: 0
@@ -23,24 +64,9 @@ int main(void)
 	puts("List content\n");
 	print_list(my_list);
 
-	/* insert some more */
-	insert(my_list, 1, 4521);
-	insert(my_list, 4, 1);
-	insert(my_list, -3, 78);
-	append(my_list, 100);
-	insert(my_list, -4, 500);
+	perform_insertions(my_list);
 
-	/* print the updated list */
-	puts("\nAfter insertion\n");
-	print_list(my_list);
-
-	puts("\nAfter pop operation\n");
-	printf("Element [%d] was popped from the list\n", pop(my_list));
-	print_list(my_list);
-
-	puts("\nRemoving data\n");
-	rm(my_list, 5);
-	print_list(my_list);
+	perform_deletions(my_list);
 
 	/* we are done, clean up please */
 	clear(my_list);
