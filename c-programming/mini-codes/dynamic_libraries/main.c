e423b685f37e0e8e199a1fc7b18cb72e7c115445
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-10 23:09:08 +0000

    Added C driver code and header file

---
+++ c-programming/mini-codes/dynamic_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	char *str = "maxwell";
+	printf("%s\n", to_title(str));
+
+	return (0);
+}
---
+++ c-programming/mini-codes/dynamic_libraries/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+
+char to_lower(char letter);
+int is_lower(char *str);
+char to_upper(char letter);
+char *to_title(char *str);
+
+#endif
