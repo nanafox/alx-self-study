58e540d641293a470163eeddd96b8b22b6e82daf
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-10 23:11:06 +0000

    Quick typo fix

--- c-programming/mini-codes/dynamic_libraries/test.py
+++ c-programming/mini-codes/dynamic_libraries/test.py
@@ -12,7 +12,7 @@ print("{:d} + {:d} = {:d}".format(num1, num2, utils.add(num1, num2)))
 
 sentence = "testing out my shared library"
 
-# First had to ensure I was accouting for the data types the function would
+# First had to ensure I was accounting for the data types the function would
 # expect and return as well. After this, my result was as I expected
 
 utils.to_title.argtypes = [ctypes.c_char_p]
