47fd918089de5c111826933e8d6919b53b8cc6ad
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-11 06:42:57 +0000

    Added few more lines to test

--- c-programming/mini-codes/dynamic_libraries/main.c
+++ c-programming/mini-codes/dynamic_libraries/main.c
@@ -1,10 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - driver code to test shared library
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	char *str = "maxwell";
+
 	printf("%s\n", to_title(str));
+	printf("%d\n", is_lower(str));
+	printf("%d\n", is_lower(""));
+	printf("%d\n", is_lower(NULL));
+	printf("%d\n", is_lower("this Is not All Lowercase"));
 
 	return (0);
 }
--- c-programming/mini-codes/dynamic_libraries/test.py
+++ c-programming/mini-codes/dynamic_libraries/test.py
@@ -9,7 +9,6 @@ num2 = 45
 
 print("{:d} + {:d} = {:d}".format(num1, num2, utils.add(num1, num2)))
 
-
 sentence = "testing out my shared library"
 
 # First had to ensure I was accounting for the data types the function would
@@ -17,6 +16,14 @@ sentence = "testing out my shared library"
 
 utils.to_title.argtypes = [ctypes.c_char_p]
 utils.to_title.restype = ctypes.c_char_p
+utils.is_lower.argtypes = [ctypes.c_char_p]
 
 # let's test the to_title function now
 print(utils.to_title(sentence.encode()).decode())
+
+# now let's check the is_lower function -> it checks if a string is all
+# lowecase
+print(utils.is_lower(sentence.encode()))
+print(utils.is_lower(b"some Other sentence I have"))
+print(utils.is_lower(b""))
+print(utils.is_lower(None))
