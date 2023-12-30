405aa2e90ddbfe53bd8528815940842b00d575a5
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-30 18:42:32 +0000

    Updated code documentation and tested __repr__

--- Python/sessions/OOP/driver.py
+++ Python/sessions/OOP/driver.py
@@ -3,17 +3,21 @@
 from person import Person
 
 
+# create two instances from the person class
 person_1 = Person("John Doe", "IT Systems Specialist", 10_000_00)
 person_2 = Person("Bob Manny", "Project Manager", 10_992.54)
 
+# print the string representation
 print(person_1)
 print()
 print(person_2)
 
+# modify the pay for person_2
 print()
 person_2.pay = 20_000_000
 print(person_2)
 
+# change the first and last name of person_2
 person_2.first_name = "Lina"
 person_2.last_name = "Banks"
 print()
@@ -21,6 +25,10 @@ print(person_2)
 
 print()
 try:
+    # try using whitespaces only as name
     person_3 = Person("     ")
 except Exception as err:
     print(err)
+
+# print debug-worthy information about the instance
+print(person_1.__repr__())
