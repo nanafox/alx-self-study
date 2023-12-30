ae29596b807e72d25f3ddaddda89b13717f1b27e
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-12-30 13:27:53 +0000

    Added a simple driver code to use Person class

---
+++ Python/sessions/OOP/driver.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from person import Person
+
+
+person_1 = Person("John Doe", "IT Systems Specialist", 10_000_00)
+person_2 = Person("Bob Manny", "Project Manager", 10_992.54)
+
+print(person_1)
+print()
+print(person_2)
+
+print()
+person_2.pay = 20_000_000
+print(person_2)
+
+person_2.first_name = "Lina"
+person_2.last_name = "Banks"
+print()
+print(person_2)
