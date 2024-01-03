5ecd3d82fc1559e873f9ef0436d9312cc31fba75
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-01-03 05:38:25 +0000

    Modified to try out the Manager class and polymorphism

--- Python/sessions/OOP/driver.py
+++ Python/sessions/OOP/driver.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-from re import M
 from person import Employee, Manager, Person
 
 
@@ -9,44 +8,54 @@ person_1 = Person("John Doe", "IT Systems Specialist", 10_000_00)
 person_2 = Person("Bob Manny", "Project Manager", 10_992.54)
 
 # print the string representation
-print(person_1)
-print()
-print(person_2)
+# print(person_1)
+# print()
+# print(person_2)
 
 # modify the pay for person_2
-print()
+# print()
 person_2.pay = 20_000_000
-print(person_2)
+# print(person_2)
 
 # change the first and last name of person_2
 person_2.first_name = "Lina"
 person_2.last_name = "Banks"
-print()
-print(person_2)
+# print()
+# print(person_2)
 
-print()
-try:
+# print()
+# try:
     # try using whitespaces only as name
-    person_3 = Person("     ")
-except Exception as err:
-    print(err)
+    # person_3 = Person("     ")
+# except Exception as err:
+    # print(err)
 
 # print debug-worthy information about the instance
-print(person_1.__repr__())
+# print(person_1.__repr__())
 
-# let's work with the Employee class
-print()
 emp_1 = Employee("Randy Olsen", "Systems Support", 150_000, "Network/Systems")
-print(emp_1)
-print(emp_1.__repr__())
+# print(emp_1)
+# print(emp_1.__repr__())
 
 emp_2 = Employee("Clark Kent", "DevOps Engineer", 108_000, "DevOps")
-print()
 
 # Manager class
 mgr_1 = Manager("Bill Doors", 200_0000, (emp_1, emp_2))
 print(mgr_1)
 
+print("\nManager 1's team")
 mgr_1.team()
 
 print(mgr_1.__repr__())
+
+
+mgr_2 = Manager("Rose Banks", 300_000, (emp_1, mgr_1))
+print()
+print(mgr_2)
+
+print("\nManager 2's team")
+
+mgr_2.team()
+
+# this shows a lot of the concepts of polymorphism
+print(mgr_2.__repr__())
