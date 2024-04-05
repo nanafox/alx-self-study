39c6510709b589d75fc246a447a0328a045aaf06
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-05 23:09:58 +0000

    Refactor code so it's Rubocop-compliant

--- Ruby/extensions.rb
+++ Ruby/extensions.rb
@@ -1,19 +1,20 @@
 #!/usr/bin/env ruby
 
 file_extensions = {
-  :gif => "image/gif",
-  :jpg => "image/jpeg",
-  :jpeg => "image/jpeg",
-  :png => "image/png",
-  :pdf => "application/pdf",
-  :txt => "text/plain",
-  :zip => "application/zip",
+  gif: 'image/gif',
+  jpg: 'image/jpeg',
+  jpeg: 'image/jpeg',
+  png: 'image/png',
+  pdf: 'application/pdf',
+  txt: 'text/plain',
+  zip: 'application/zip'
 }
 
-print "Enter the filename: "
+print 'Enter the filename: '
 filename = gets.chomp.downcase
-extension = filename.scan(/\.\w+$/)[0] or raise "No file extension found"
+extension = filename.scan(/\.\w+$/)[0] \
+  or raise 'No file extension found'
 
 puts file_extensions.fetch(
-  extension.sub(".", "").to_sym, "application/octet-stream"
+  extension.sub('.', '').to_sym, 'application/octet-stream'
 )
