fe883bd68b2b26fa9be59d5d9e0a60eccf0e2883
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-16 21:56:15 +0000

    Add Car class implementation in Ruby

---
+++ Ruby/car.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+# Defines a Car class
+class MyCar
+  attr_accessor :color
+  attr_reader :year
+
+  def initialize(name:, year:, color:, model:)
+    @name = name
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+
+  def to_s
+    "This is a #{@color} #{@model} #{@name}, manufactured in #{@year}."
+  end
+
+  def current_speed
+    puts "Your current speed is #{@current_speed} mph"
+  end
+
+  def speed_up(speed = 10)
+    @current_speed += speed
+    puts "Speeding up to #{@speed}mph"
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "Applying the brakes now. Decelerating by #{number} mph"
+  end
+
+  def shutdown
+    puts 'Shutting down the car for charging.'
+  end
+
+  def spray_paint(paint_color)
+    self.color = paint_color
+    puts "Your #{@model} has been painted #{color} now"
+  end
+end
+
+lumina = MyCar.new(
+  name: 'Chevy', year: 1997, model: 'chevy lumina', color: 'white'
+)
+
+lumina.speed_up(20)
+lumina.current_speed
+lumina.speed_up(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.brake(20)
+lumina.current_speed
+lumina.shutdown
+lumina.current_speed
+
+puts lumina
+
+# check accessors
+puts lumina.color
+puts lumina.year
+
+lumina.color = 'blue'
+
+puts lumina
+
+# use the `spray_paint` method
+
+lumina.spray_paint('red')
+
+puts lumina
