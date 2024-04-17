8ed364939489ae1676365360243092c837701949
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-17 22:02:46 +0000

    Refactor Car class to Vehicle class and add VehicleFunctions module

--- Ruby/car.rb
+++ Ruby/car.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
+require_relative 'vehicle_functions'
+
 # Defines a Car class
-class MyCar
+class Vehicle
+  include VehicleFunctions
+  @number_of_vehicles = 0
+
+  class << self
+    attr_accessor :number_of_vehicles
+  end
+
   attr_accessor :color
   attr_reader :year
 
   # calculates the gas mileage of any car
   def self.gas_mileage(gallons, miles)
-    puts "You go #{miles / gallons} miles per gallon of gas"
+    puts "You go #{miles / gallons} miles per gallon of gas."
   end
 
   def initialize(name:, year:, color:, model:)
@@ -16,6 +25,8 @@ class MyCar
     @color = color
     @model = model
     @current_speed = 0
+    @fuel_level = 0
+    Vehicle.number_of_vehicles += 1
   end
 
   def to_s
@@ -26,56 +37,73 @@ class MyCar
     puts "Your current speed is #{@current_speed} mph"
   end
 
-  def speed_up(speed = 10)
-    @current_speed += speed
-    puts "Speeding up to #{@current_speed}mph"
-  end
-
-  def brake(number)
-    @current_speed -= number
-    puts "Applying the brakes now. Decelerating by #{number} mph"
-  end
-
-  def shutdown
-    puts 'Shutting down the car for charging.'
-  end
-
   def spray_paint(paint_color)
     self.color = paint_color
     puts "Your #{@model} has been painted #{color} now"
   end
+
+  def age
+    compute_age
+  end
+
+  private
+
+  def compute_age
+    Time.new.year - year
+  end
 end
 
+# Defines a car class
+class MyCar < Vehicle
+  include MyCarMixin
+  USAGE = 'For leisure and pleasure.'
+end
+
+class MyTruck < Vehicle
+  USAGE = 'For heavy-duty work.'
+end
+
+# cars
 lumina = MyCar.new(
   name: 'Chevy', year: 1997, model: 'chevy lumina', color: 'white'
 )
 
-lumina.speed_up(20)
-lumina.current_speed
-lumina.speed_up(20)
-lumina.current_speed
-lumina.brake(20)
-lumina.current_speed
-lumina.brake(20)
-lumina.current_speed
-lumina.shutdown
-lumina.current_speed
+MyCar.new(
+  name: 'Toyota', year: 2018, model: 'Toyota Camry', color: 'black'
+)
 
-puts lumina
+MyCar.new(
+  name: 'Honda', year: 2020, model: 'Honda Accord', color: 'silver'
+)
 
-# check accessors
-puts lumina.color
-puts lumina.year
+# trucks
+MyTruck.new(
+  name: 'Ford', year: 2005, model: 'Ford F-150', color: 'blue'
+)
 
-lumina.color = 'blue'
+MyTruck.new(
+  name: 'Chevy', year: 2010, model: 'Chevy Silverado', color: 'red'
+)
 
-puts lumina
+puts Vehicle.number_of_vehicles
 
-# use the `spray_paint` method
+puts "The Lumina is #{lumina.age} year(s) old."
+lumina.start_engine
 
-lumina.spray_paint('red')
+lumina.refuel(40)
+lumina.refuel(20)
+lumina.accelerate(10)
+lumina.accelerate(30)
+lumina.decelerate(20)
+lumina.turn_on_air_conditioning
 
-puts lumina
+lumina.stop_engine
 
-# made up miles and gallons
-MyCar.gas_mileage(50, 900)
+puts '---MyCar method lookup---'
+puts MyCar.ancestors
+
+puts '---MyTruck method lookup---'
+puts MyTruck.ancestors
+
+puts '---Vehicle method lookup---'
+puts Vehicle.ancestors
