62102f0e16d59a511a944b4eb087cf3ff2cc5f21
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-24 15:54:38 +0000

    Add class method to Student for finding a student by name

--- Ruby/student.rb
+++ Ruby/student.rb
@@ -2,6 +2,23 @@
 
 # A simple class for a student
 class Student
+  class << self
+    # A class method to find a student by name
+    def find_by_name(name)
+      students.find { |student| student.name == name }
+    end
+
+    # A class method to keep track of all students
+    def students
+      @students ||= []
+    end
+
+    # A class method to add a student to the list
+    def add_student(student)
+      students.push(student)
+    end
+  end
+
   attr_accessor :name
   attr_writer :grade
 
@@ -10,6 +27,7 @@ class Student
     raise TypeError unless grade.is_a? String
 
     self.grade = grade.upcase
+    self.class.add_student(self)
   end
 
   #
@@ -26,6 +44,10 @@ class Student
     grade < other.grade
   end
 
+  def to_s
+    "#{name} has a grade of #{grade}"
+  end
+
   protected
 
   attr_reader :grade
@@ -35,3 +57,4 @@ joe = Student.new(name: 'Joe', grade: 'A')
 bob = Student.new(name: 'Bob', grade: 'C')
 
 puts "Well done, #{joe.name}!" if joe.better_grade_than?(bob)
+puts Student.find_by_name('Joe')
